d643466ed1eabe865ea60f110f725f4f0240febc
Colton Gray <78562164+GrayoftheGalaxy@users.noreply.github.com>
2023-12-02 16:55:53 -0600

    Add files via upload
    Finished options menu, added scanner base menu. Will add progress bar, need to add functionality

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 class Menu:
     #Closes the program
@@ -9,9 +10,63 @@ class Menu:
     def __init__(self, window):
         frame = Frame(window, pady=20, padx=20)
         title = Label(frame, text="Evil-Twin Scanner", font=('Lucida Sans', 32), pady=10)
-        start_button = Button(frame, text="Start Scanning", font=('Lucida Sans', 16), bg='#d6d6d6', activebackground='#d6d6d6', pady=(10), command= lambda: Menu.scan(window)) 
+        start_button = Button(frame, text="Start Scanning", font=('Lucida Sans', 16), bg='#d6d6d6', activebackground='#d6d6d6', pady=(10), command= lambda: Menu.options(window)) 
         exit_button = Button(frame, text="Close", font=('Lucida Sans', 16), bg='#d6d6d6', activebackground='#d6d6d6', pady=(10), command= lambda: Menu.exit(window))
         title.pack()
         start_button.pack()
         exit_button.pack()
         frame.pack()
+
+    def options(window):
+        try:
+            for key in window.children:
+                window.children[key].destroy()
+        except:
+            pass
+        finally:
+            email = StringVar()
+            timer = StringVar()
+            frame = Frame(window, pady=20, padx=20)
+            title = Label(frame, text='Settings', font=('Lucida Sans', 24), pady=(5), padx=100)
+            login_div = Frame(frame, pady=10)
+            details = Label(login_div, text = "Please input an email if you would like an email to be sent when we detect an Evil Twin.", font=("Lucida Sans", 10), pady=3)
+            more_details = Label(login_div, text = "If you don't want to receive an email, leave blank.", font = ("Lucida Sans", 10), pady=3)
+            email_label = Label(login_div, text="Email",font=('Lucida Sans', 16), pady=3)
+            timer_details = Label(login_div, text = "Please input how often the a scan should occur (in seconds).", font=("Lucida Sans", 10), pady=3)
+            begin_button = Button(frame, text="Start Scanning", font=("Lucida Sans", 16), bg='#d6d6d6', activebackground='#d6d6d6', pady=(10), command= lambda: Menu.start_scanner(window, email.get()))
+            details.pack()
+            more_details.pack()
+            email = Entry(login_div, textvariable=email, width=50)
+            timer = Entry(login_div, textvariable=timer, width=10)
+            email_label.pack()
+            email.pack()
+            timer_details.pack()
+            timer.pack()
+            title.pack()
+            login_div.pack()
+            begin_button.pack()
+            frame.pack()
+
+    def start_scanner(window, email):
+        try:
+            for key in window.children:
+                window.children[key].destroy()
+        except:
+            pass
+        finally:
+            frame = Frame(window, pady=20, padx=20)
+            title = Label(frame, text="Now scanning!", font=("Lucida Sans", 24), pady=5, padx=100)
+            text = Label(frame, text="The scanner is now running. Please feel free to leave the program in the background.", font=("Lucida Sans", 10), pady=3)
+            moreText = Label(frame, text="If an Evil Twin is encountered, the program will give you an alert.", font=("Lucida Sans", 10), pady=3)
+            evenMoreText = Label(frame, text="If chosen, it will also send an email to: " + email, font = ("Lucida Sans", 10), pady=3)
+            exit_button = Button(frame, text="Close Program", font=('Lucida Sans', 16), bg='#d6d6d6', activebackground='#d6d6d6', pady=(10), command= lambda: Menu.exit(window))
+            title.pack()
+            text.pack()
+            moreText.pack()
+            evenMoreText.pack()
+            exit_button.pack()
+            frame.pack()
+            
+            
+            
+        
